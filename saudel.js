ac86df8c0eb491b744adbf78b939369131d360d3
SAudelOG <audel91@gmail.com>
2015-07-11 11:30:20 -0500

    http server running

--- saudel.js
+++ saudel.js
@@ -1,5 +1,15 @@
 (function(){
   'use strict'
 
-  var express = require('express');
+  var express = require('express'),
+      http = require('http'),
+      app = express();
+
+  app.set('port', process.env.PORT || 3000);
+
+  http.createServer(app).listen(app.get('port'),function(){
+    console.log('Express started in: ' + app.get('env') +
+      ' mode on http://localhost: ' + app.get('port') +
+      '; press Ctrl-C to terminate.');
+  });
 })();
