739c14c5d4d8e068c661918a553edd58967438d7
SAudelOG <audel91@gmail.com>
2015-07-11 23:08:11 -0500

    set up handlebars engine

--- saudel.js
+++ saudel.js
@@ -4,8 +4,29 @@
   var express = require('express'),
       http = require('http'),
       app = express();
-
+  //get enviroment port or set default 3000
   app.set('port', process.env.PORT || 3000);
+
+  //public resourcer
+  app.use(express.static(__dirname + '/pulic'));
+
+  //set up handlebars view engine
+  var handlebars = require('express3-handlebars').create({
+    default: 'main',
+    hepers: {
+      section: function(name, options){
+          if(!this._sections) this._sections = {};
+          this._sections[name] = options.fn(this);
+          return null;
+      }
+    }
+  });
+  app.engine('handlebars', handlebars.engine);
+  app.set('view engine', 'handlebars');
+
+  app.get('/', function(req, res){
+    res.render('home');
+  });
 
   http.createServer(app).listen(app.get('port'),function(){
     console.log('Express started in: ' + app.get('env') +
