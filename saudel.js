2b25fafc1730cdd904cbb40f8fcfe576c163d762
SAudelOG <audel91@gmail.com>
2015-07-12 00:15:38 -0500

    jquery seccions fixed

--- saudel.js
+++ saudel.js
@@ -13,7 +13,7 @@
   //set up handlebars view engine
   var handlebars = require('express3-handlebars').create({
     defaultLayout: 'main',
-    hepers: {
+    helpers: {
       section: function(name, options){
           if(!this._sections) this._sections = {};
           this._sections[name] = options.fn(this);
